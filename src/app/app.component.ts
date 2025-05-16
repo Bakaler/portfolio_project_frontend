9781359a9e42c93c71071ca474ad23d625a70d6b
alexander baker <alexbaker5795@gmail.com>
2025-05-16 15:05:59 -0500

    Initial Commit / Weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './weather.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio_project_frontend';
+  forecast: any[] = [];
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.getForecast().subscribe(data => {
+      this.forecast = data;
+      console.log('Weather forecast from backend:', this.forecast);
+    });
+  }
 }
